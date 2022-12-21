4d6826882f3e1d32b52047b44aabda47a4a64655
Shivamkumar <shivammessi07@gmail.com>
2022-12-21 20:01:55 +0530

    new changes

--- contact_list/index.js
+++ contact_list/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const path = require('path');
+const { next } = require('process');
 const port = 3000;
 
 const app = express();
+
+// app.use(function(req, res, next){
+//     console.log("middleware 1` is called");
+//     next();
+// })
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded());
+app.use(express.static('assets'))
 
 var contactList = [
     {
@@ -19,21 +29,35 @@ var contactList = [
     }
 ]
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.get('/practice', function (req, res) {
+    return res.render('practice',
+        {
+            title: "I am flying"
+        });
+});
+
+
 
 app.get('/', function (req, res) {
     return res.render('home', {
-        title: "I am flying",
+        title: "Contatc List",
         contact_List: contactList
     });
-})
-app.get('/practice', function (req, res) {
-    return res.render('practice', { title: "I am flying" });
 });
 
-app.post('/create-contact', function(req, res){
-    return res.redirect('/practice')
+
+app.post('/create-contact', function (req, res) {
+    // return res.redirect('/practice')
+    contactList.push({
+        name: req.body.name,
+        phone: req.body.phone
+    });
+    res.redirect('/');
+})
+
+app.get('/delete-contact/:phone', function(req, res){
+    console.log(req.params);
+    let phone = req.params.photo;
 })
 
 app.listen(port, function (err) {
@@ -42,3 +66,15 @@ app.listen(port, function (err) {
     }
     console.log("Yay! my express server is running on port: ", port)
 })
+
+app.get('/delete-contact', function(req, res){
+    console.log(req.query);
+    let phone = req.query.phone;
+    let contactindex = contactList.findIndex(contact => contact.phone == phone);
+
+    if(contactindex != -1){
+        contactList.splice(contactindex, 1);
+    }
+
+    return res.redirect('back');
+});
